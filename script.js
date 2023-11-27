122476284fcc722cf8d6da51abea5028a242a15a
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2023-11-27 15:07:25 -0500

    initialize generatePassword function, add prompt for length

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Generate password
+function generatePassword() {
+  // WHEN prompted for password criteria
+  // THEN I select which criteria to include in the password
+  // WHEN prompted for the length of the password
+  // THEN I choose a length of at least 8 characters and no more than 128 characters
+  // Get the password length from the user:
+  let passwordLength;
+  do {
+    // Prompt the user for their input (and cast as a number)
+    passwordLength = +prompt("Choose a password length (enter a number between 8 and 128):");
+    // Inform the user if there is a problem with their input
+    if (Number.isNaN(passwordLength)) {
+      alert("Please enter a number.");
+    } else if (!Number.isInteger(passwordLength)) {
+      alert("Please enter a whole number.");
+    } else if (passwordLength < 8 || passwordLength > 128) {
+      alert("Please enter a number between 8 and 128.");
+    }
+    // Keep prompting until they give an appropriate value
+  } while (Number.isNaN(passwordLength) || !Number.isInteger(passwordLength) || passwordLength < 8 || passwordLength > 128);
+
+  // WHEN asked for character types to include in the password
+  // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+  // WHEN I answer each prompt
+  // THEN my input should be validated and at least one character type should be selected
+  // WHEN all prompts are answered
+  // THEN a password is generated that matches the selected criteria
+  // WHEN the password is generated
+  // THEN the password is either displayed in an alert or written to the page
+}
